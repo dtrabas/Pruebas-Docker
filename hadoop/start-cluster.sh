0aa986e69dfd111082f6e1e06ce7097749112890
Daniel Trabas <dtrabas@gmail.com>
2022-05-02 01:35:40 +0200

    Añadido cluster hadoop

---
+++ hadoop/start-cluster.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# create base hadoop cluster docker image
+docker build -f base/Dockerfile -t dtrabas/hadoop-cluster-base:latest base
+
+# create master node hadoop cluster docker image
+docker build -f master/Dockerfile -t dtrabas/hadoop-cluster-master:latest master
+
+# the default node number is 3
+N=${1:-3}
+
+docker network create --driver=bridge hadoop &> /dev/null
+
+# start hadoop slave container
+i=1
+while [ $i -lt $N ]
+do
+	docker rm -f hadoop-slave$i &> /dev/null
+	echo "start hadoop-slave$i container..."
+	docker run -itd \
+	                --net=hadoop \
+	                --name hadoop-slave$i \
+	                --hostname hadoop-slave$i \
+	                dtrabas/hadoop-cluster-base
+	i=$(( $i + 1 ))
+done 
+
+# start hadoop master container
+docker rm -f hadoop-master &> /dev/null
+echo "start hadoop-master container..."
+docker run -itd \
+                --net=hadoop \
+                -p 50070:50070 \
+                -p 8088:8088 \
+                --name hadoop-master \
+                --hostname hadoop-master \
+				-v /${PWD}/data:/data \
+                dtrabas/hadoop-cluster-master
+
+# Nota: en el comando anterior, ponemos <-v /${PWD}/data:/data \> para que funcione el script en git-bash
+
+# get into hadoop master container
+docker exec -it hadoop-master bash
